8aca61eadecfc7ea232a6d719cb98afc61988153
MaxFreeStyleCode <143237748+MaxFreeStyleCode@users.noreply.github.com>
2023-09-12 20:45:25 -0400

    Creation of the hidden menu

--- js/dynamic-nav-header.js
+++ js/dynamic-nav-header.js
@@ -1,5 +1,8 @@
 let subPostStylesMainMenuAppear = false;
 let subCategoriesMainMenuAppear = false;
+let subPostStylesHiddenMenuAppear = false;
+let subCategoriesHiddenMenuAppear = false;
+let hiddenMenuAppear = false;
 
 let butDisplaySearchInputs = document.getElementsByClassName("search_sect");
 let backSearchSect = document.getElementById("back_search_sect");
@@ -17,6 +20,19 @@ let subPostStylesMainMenu = document.getElementById(
   "sub_post_styles_main_menu"
 );
 let subCategoriesMainMenu = document.getElementById("sub_categories_main_menu");
+let hiddenMenu = document.getElementById("hidden_menu");
+let displaySubPostStylesHiddenMenu = document.getElementById(
+  "display_sub_post_styles_hidden_menu"
+);
+let displaySubCategoriesHiddenMenu = document.getElementById(
+  "display_sub_categories_hidden_menu"
+);
+let subPostStylesHiddenMenu = document.getElementById(
+  "sub_post_styles_hidden_menu"
+);
+let subCategoriesHiddenMenu = document.getElementById(
+  "sub_categories_hidden_menu"
+);
 
 for (let i = 0; i < butDisplaySearchInputs.length; i++) {
   butDisplaySearchInputs[i].addEventListener("click", () => {
@@ -31,10 +47,16 @@ closeBackSearchSect.addEventListener("click", () => {
 function closeAllSubMenu() {
   subPostStylesMainMenuAppear = false;
   subCategoriesMainMenuAppear = false;
+  subPostStylesHiddenMenuAppear = false;
+  subCategoriesHiddenMenuAppear = false;
   subPostStylesMainMenu.style.opacity = "0";
   subPostStylesMainMenu.style.transform = "translateY(-700px)";
   subCategoriesMainMenu.style.opacity = "0";
   subCategoriesMainMenu.style.transform = "translateY(-700px)";
+  subPostStylesHiddenMenu.style.opacity = "0";
+  subPostStylesHiddenMenu.style.transform = "translateY(-700px)";
+  subCategoriesHiddenMenu.style.opacity = "0";
+  subCategoriesHiddenMenu.style.transform = "translateY(-700px)";
 }
 
 displaySubPostStylesMainMenu.addEventListener("click", () => {
@@ -59,4 +81,42 @@ displaySubCategoriesMainMenu.addEventListener("click", () => {
     subCategoriesMainMenu.style.transform = "translateY(-700px)";
   }
   subCategoriesMainMenuAppear = !subCategoriesMainMenuAppear;
+});
+
+displaySubPostStylesHiddenMenu.addEventListener("click", () => {
+  if (!subPostStylesHiddenMenuAppear) {
+    closeAllSubMenu();
+    subPostStylesHiddenMenu.style.opacity = "1";
+    subPostStylesHiddenMenu.style.transform = "translateY(0px)";
+  } else {
+    subPostStylesHiddenMenu.style.opacity = "0";
+    subPostStylesHiddenMenu.style.transform = "translateY(-700px)";
+  }
+  subPostStylesHiddenMenuAppear = !subPostStylesHiddenMenuAppear;
+});
+
+displaySubCategoriesHiddenMenu.addEventListener("click", () => {
+  if (!subCategoriesHiddenMenuAppear) {
+    closeAllSubMenu();
+    subCategoriesHiddenMenu.style.opacity = "1";
+    subCategoriesHiddenMenu.style.transform = "translateY(0px)";
+  } else {
+    subCategoriesHiddenMenu.style.opacity = "0";
+    subCategoriesHiddenMenu.style.transform = "translateY(-700px)";
+  }
+  subCategoriesHiddenMenuAppear = !subCategoriesHiddenMenuAppear;
+});
+
+document.addEventListener("scroll", (e) => {
+  if (window.scrollY > 300) {
+    if (!hiddenMenuAppear) {
+      hiddenMenuAppear = true;
+      hiddenMenu.style.transform = "translateY(0px)";
+    }
+  } else {
+    if (hiddenMenuAppear) {
+      hiddenMenuAppear = false;
+      hiddenMenu.style.transform = "translateY(-400px)";
+    }
+  }
 });

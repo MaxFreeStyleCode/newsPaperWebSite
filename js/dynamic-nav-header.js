d7b957c5605419ced4c283eaa219e53912b8742d
MaxFreeStyleCode <143237748+MaxFreeStyleCode@users.noreply.github.com>
2023-09-12 20:20:46 -0400

    Creation of the back-search-section

---
+++ js/dynamic-nav-header.js
@@ -0,0 +1,20 @@
+let butDisplaySearchInputs = document.getElementsByClassName("search_sect");
+let backSearchSect = document.getElementById("back_search_sect");
+let closeBackSearchSect = document.getElementById("close_back_search_sect");
+let responsiveHeaderSearch = document.getElementById(
+  "responsive_header_search"
+);
+
+for (let i = 0; i < butDisplaySearchInputs.length; i++) {
+  butDisplaySearchInputs[i].addEventListener("click", () => {
+    backSearchSect.style.transform = "translateY(0vh)";
+  });
+}
+
+closeBackSearchSect.addEventListener("click", () => {
+  backSearchSect.style.transform = "translateY(-110vh)";
+});
+
+responsiveHeaderSearch.addEventListener("click", () => {
+  backSearchSect.style.transform = "translateY(0vh)";
+});
